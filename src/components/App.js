f5492ff526e2eb5d0e9504c72fc98600ef8e60e9
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-02-18 14:10:09 +0800

    [#1] - Cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,72 +1,25 @@
-// require('normalize.css/normalize.css');
+// require('normalize.css/normalize.css')
 import './App.css'
 
 import React from 'react'
-import HomePage from './page/HomePage'
 import Radar from './svg/Radar'
 import ListComponent from './Radar/RadarList'
 
-const UUID = require('uuid-js');
+const UUID = require('uuid-js')
 
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {page: 'create', radius: 300, arr: []}
+    this.state = {
+      page: 'create', radius: 300, arr: []
+    }
     this.screen2Cartesian = this.screen2Cartesian.bind(this)
   }
 
-  navigateToRadarPage() {
-    this.setState({page: 'create', radius: 300, arr:[]})
-  }
-
-  render() {
-    if (this.state.page == 'create') {
-      let items = this.state.arr.map(this.points2Items.bind(this))
-
-      let items1st = items.filter((item) => {
-        return item.x > 0 && item.y > 0
-      }).map(this.cartesian2Screen.bind(this))
-      let items2nd = items.filter((item) => {
-        return item.x < 0 && item.y > 0
-      }).map(this.cartesian2Screen.bind(this))
-      let items3rd = items.filter((item) => {
-        return item.x < 0 && item.y < 0
-      }).map(this.cartesian2Screen.bind(this))
-      let items4th = items.filter((item) => {
-        return item.x > 0 && item.y < 0
-      }).map(this.cartesian2Screen.bind(this))
-      let points = this.state.arr
-      return (
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <ListComponent title="Techniques" items={items2nd} key={`${UUID.create().toString()}`}/>
-            </div>
-            <div className="col-md-4 col-md-offset-4">
-              <ListComponent title="Tools" items={items1st} key={`${UUID.create().toString()}`}/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12">
-              <Radar radius={this.state.radius}
-                points={points} didChangedPoints={this.didChangedPoints.bind(this)}/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-4">
-              <ListComponent title="Platforms" items={items3rd} key={`${UUID.create().toString()}`}/>
-            </div>
-            <div className="col-md-4 col-md-offset-4">
-              <ListComponent title="Languages & Frameworks" items={items4th} key={`${UUID.create().toString()}`}/>
-            </div>
-          </div>
-        </div>
-      )
-    }
-
-    return (
-      <HomePage showRadar={this.navigateToRadarPage.bind(this)}/>
-    )
+  didChangedPoints(points) {
+    this.setState({
+      page: 'create', margin: 60, radius: 250, arr: points
+    })
   }
 
   cartesian2Screen(point) {
@@ -91,14 +44,51 @@ class App extends React.Component {
     }
   }
 
-  didChangedPoints(points) {
-    this.setState({page: 'create', margin: 60, radius: 250, arr: points})
-  }
+  render() {
+    let items = this.state.arr
+      .map((point) => ({...point, index: this.state.arr.indexOf(point) + 1}))
 
-  points2Items(point) {
-    let item = point
-    item.index = this.state.arr.indexOf(item) + 1
-    return item
+
+    let items1st = items
+      .filter((item) => item.x > 0 && item.y > 0)
+      .map((point) => this.cartesian2Screen(point))
+
+    let items2nd = items.filter((item) => {
+      return item.x < 0 && item.y > 0
+    }).map(this.cartesian2Screen.bind(this))
+    let items3rd = items.filter((item) => {
+      return item.x < 0 && item.y < 0
+    }).map(this.cartesian2Screen.bind(this))
+    let items4th = items.filter((item) => {
+      return item.x > 0 && item.y < 0
+    }).map(this.cartesian2Screen.bind(this))
+    let points = this.state.arr
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <ListComponent title="Techniques" items={items2nd} key={`${UUID.create().toString()}`}/>
+          </div>
+          <div className="col-md-4 col-md-offset-4">
+            <ListComponent title="Tools" items={items1st} key={`${UUID.create().toString()}`}/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <Radar radius={this.state.radius}
+                   points={points} didChangedPoints={this.didChangedPoints.bind(this)}/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <ListComponent title="Platforms" items={items3rd} key={`${UUID.create().toString()}`}/>
+          </div>
+          <div className="col-md-4 col-md-offset-4">
+            <ListComponent title="Languages & Frameworks" items={items4th} key={`${UUID.create().toString()}`}/>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
